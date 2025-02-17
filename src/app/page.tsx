0043a189f273b7734d28814e09044400063f25df
Chisom <nwikechisom77@gmail.com>
2025-02-17 15:52:44 +0100

    nowsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 
 import Overview from "@/components/home/Overview";
-import Template from "./Template"
-
 export default function Home() {
  
   return (
     <div>
       <Overview />
-      {/* <Template /> */}
     </div>
   );
 }
